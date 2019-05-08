e5f61ed696c0bbe98074c1d76a7c657493529939
Pedro Demarchi Gomes <pdg16@inf.ufpr.br>
2019-05-08 09:20:08 -0300

    Contando miss

--- main.cr
+++ main.cr
@@ -1,15 +1,19 @@
 require "./tlb.cr"
 path_file_addresses = "lista_enderecos.txt"
-path_file_pt = "lista_enderecos.txt"
+path_file_pt = "pt.txt"
 
 # Create empty TLB
 tlb = Tlb.new 8
 
 # Fill PT
+tlb.fillPt(path_file_pt)
 
-
-File.each_line path_file do |line|
-    address = line.to_u32
+File.each_line path_file_addresses do |line|
+    address = line.to_u64(16)
     puts "#{address}"
     tlb.translate(address)
 end
+
+print "Miss counter = #{tlb.missCounter}\n\n"
+
+print tlb.printTlb
