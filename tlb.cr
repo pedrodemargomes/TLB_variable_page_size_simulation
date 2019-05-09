2374ac639f1f457cb8220b2a872505c8fe2355da
Pedro Demarchi Gomes <pedrodemargomes@gmail.com>
2019-05-09 20:18:51 -0300

    WIP: implementa LRU

--- tlb.cr
+++ tlb.cr
@@ -1,8 +1,8 @@
 
 struct TlbEntry
-	property tagMask, tag_offset, ppn, valid
+	property tagMask, tag_offset, ppn, valid, ptIndex
 
-	def initialize(@tagMask : UInt64, @tag_offset : UInt64, @ppn : UInt64, @valid : Bool)
+	def initialize(@tagMask : UInt64, @tag_offset : UInt64, @ppn : UInt64, @valid : Bool, @ptIndex : UInt64)
 
 	end
 
@@ -13,20 +13,40 @@ class Tlb
 
 	def initialize(@length : UInt64)
 		@tlb = [] of TlbEntry
-		@length.times.each{|i| @tlb << TlbEntry.new(0,0,0, false) }
+		@length.times.each{|i| @tlb << TlbEntry.new(0,0,0, false,i) }
 		@pt = [] of TlbEntry
 		@missCounter = 0
+		@lru = [] of UInt64
+		@length.times.each{|i| @lru << i }
 	end
 
-	def insertTlb(newEntryTlb : TlbEntry)
+	def insertTlbRand(newEntryTlb : TlbEntry)
 		rand =  Random.new.rand(@length)
 		@tlb[rand] = newEntryTlb
+	end
+
+	def insertTlbLRU(newEntryTlb : TlbEntry, ptIndex : UInt64)
+		indexInTlb = @tlb.index{|x| x.ptIndex == @lru.first }
+		print indexInTlb
+		@tlb.delete_at(indexInTlb)
+		@tlb.push(newEntryTlb)
+		@lru.delete_at(0)
+		@lru.push(ptIndex)
+	end
+
+	def updateLRU(ptIndex)
+		# procura na lru o elemento com valor ptIndex e sobe ele
+		@lru.delete(ptIndex)
+		@lru.push(ptIndex)
 	end
 
 	def translate(virtualAddress : UInt64)
 		# Search address in TLB
 		@tlb.each do |entry|
 			if (entry.valid) && ((entry.tagMask & entry.tag_offset) == (entry.tagMask & virtualAddress) ) # HIT
+				# ++++++ Using LRU ++++++
+				updateLRU(entry.ptIndex)
+				# +++++++++++++++++++++++
 				return (~entry.tagMask & entry.tag_offset) + entry.ppn
 			end
 		end
@@ -34,7 +54,8 @@ class Tlb
 		@missCounter += 1
 		@pt.each do |entry|
 			if (entry.tagMask & entry.tag_offset) == (entry.tagMask & virtualAddress) # HIT
-				insertTlb(entry) # Insert address in TLB
+				insertTlbLRU(entry, entry.ptIndex) # Insert address in TLB using LRU
+				# insertTlbRand(entry) # Insert address in TLB random
 				return (~entry.tagMask & entry.tag_offset) + entry.ppn
 			end
 		end
@@ -42,14 +63,25 @@ class Tlb
 	end
 
 	def fillPt(path_file_pt)
+		count = uninitialized UInt64
+		count = 0
 		File.each_line path_file_pt do |line|
 			aux = line.split
-			@pt << TlbEntry.new(aux[1].to_u64(16), aux[0].to_u64(16), aux[2].to_u64(16), true)
+			@pt << TlbEntry.new(aux[1].to_u64(16), aux[0].to_u64(16), aux[2].to_u64(16), true, count)
+			count += 1
 		end
 	end
 
 	def printTlb()
-		print  "#{@tlb}\n\n"
+		@tlb.each_with_index do |entry, index|
+			print  "#{index}: #{entry}\n"
+		end
+	end
+
+	def printPt()
+		@pt.each_with_index do |entry, index|
+			print  "#{index}: #{entry}\n"
+		end
 	end
 
 end
