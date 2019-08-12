4ef550806aa1b42260ed886f127f13310e799c44
Pedro Demarchi Gomes <pdg16@inf.ufpr.br>
2019-08-12 18:03:12 -0300

    transfrom traces using pmap_tratado

--- pmap_transform/transform_traces.py
+++ pmap_transform/transform_traces.py
@@ -2,7 +2,19 @@ import humanfriendly
 import gzip
 import io
 import sys
+import ast
 
-with io.TextIOWrapper(io.BufferedReader(gzip.open(sys.argv[1]))) as file:
+def newSegValue(old_seg, list_seg):
+    for i in list_seg:
+        if old_seg-int(i[7],16) < humanfriendly.parse_size(i[8], binary=True): 
+            return old_seg + i[5]
+
+list_seg = []
+with open(sys.argv[1], 'r') as f:
+    for line in f:
+        list_seg.append( ast.literal_eval(line) )
+
+with io.TextIOWrapper(io.BufferedReader(gzip.open(sys.argv[2]))) as file:
     for line in file:
-        print hex(int(line.split(",")[2])) + line.split(",")[1]
+        # print hex(int(line.split(",")[2])) + line.split(",")[1]
+        print hex(newSegValue(int(line.split(",")[1],16), list_seg))
