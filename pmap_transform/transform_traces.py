86d79e0db416716e22ffe70fb1b9201e5ed9574c
Pedro Demarchi Gomes <pedrodemargomes@gmail.com>
2019-08-13 22:49:25 -0300

    add parametes and optimize code

--- pmap_transform/transform_traces.py
+++ pmap_transform/transform_traces.py
@@ -4,9 +4,14 @@ import io
 import sys
 import ast
 
+if len(sys.argv) != 3:
+	print "Numero errado de argumentos"
+	print "python transform_traces.py pmaps_tratados.txt tracefile-trace-00000.txt.gz"
+	exit(-1)
+
 def newSegValue(old_seg, list_seg):
     for i in list_seg:
-        if old_seg-int(i[7],16) < humanfriendly.parse_size(i[8], binary=True): 
+        if old_seg-i[7] < i[8]: 
             return old_seg + i[5]
 
 list_seg = []
@@ -14,7 +19,12 @@ with open(sys.argv[1], 'r') as f:
     for line in f:
         list_seg.append( ast.literal_eval(line) )
 
+for i in list_seg:
+	i[7] = int(i[7],16)
+	i[8] = humanfriendly.parse_size(i[8], binary=True)
+
 with io.TextIOWrapper(io.BufferedReader(gzip.open(sys.argv[2]))) as file:
     for line in file:
         # print hex(int(line.split(",")[2])) + line.split(",")[1]
-        print hex(newSegValue(int(line.split(",")[1],16), list_seg))
+        print hex(newSegValue(int(line.split(",")[1],16), list_seg))[2:]
+
