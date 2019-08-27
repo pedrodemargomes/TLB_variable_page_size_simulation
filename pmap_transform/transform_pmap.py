85cd16c1805b32cf8b66887aa73b5c866da5bd18
Pedro Demarchi Gomes <pdg16@inf.ufpr.br>
2019-08-26 22:01:43 -0300

    fix bug

--- pmap_transform/transform_pmap.py
+++ pmap_transform/transform_pmap.py
@@ -3,6 +3,7 @@ import sys
 import math
 import itertools
 import copy
+import ast
 
 def nextPowerOf2(n): 
 	n -= 1
@@ -53,7 +54,7 @@ if len(sys.argv) != 3:
 f = open(sys.argv[1], "r")
 list_pmap_orig = []
 for x in f:
-	list_pmap_orig.append(filter(lambda a: a != "[" and a != "]", x.split()))
+	list_pmap_orig.append(ast.literal_eval(x))
 
 list_pmap_orig.sort(cmp=cmpN)
 
