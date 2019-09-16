e24d7f12056863444c8926a9c1ccf1cf00658012
Pedro Demarchi Gomes <pedrodemargomes@gmail.com>
2019-09-16 17:49:36 -0300

    transform all traces

---
+++ pmap_transform/transform_all.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+for i in $(seq 0 2000)
+do 
+	echo "tracefile-trace-${x}.txt.zip"
+	x=$(printf "%05d" $i)
+	python ~/tlb/pmap_transform/transform_traces.py pmaps/pmaps_tratados.txt tracefile-trace-${x}.txt.zip > tracefile-trace-${i}_tratados.txt
+done
