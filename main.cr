95bce18b045d9569e61869e775fde89cf8ab53fc
Pedro Demarchi Gomes <pedrodemargomes@gmail.com>
2019-09-16 17:51:29 -0300

    add argument size of TLB

--- main.cr
+++ main.cr
@@ -1,8 +1,8 @@
 require "./tlb.cr"
 
-if ARGV.size() != 2
+if ARGV.size() != 3
 	print "Numero errado de argumentos\n"
-	print "crystal run main.cr -- lista_enderecos.txt pt.txt\n"
+	print "crystal run main.cr -- lista_enderecos.txt pt.txt tamanho_da_TLB\n"
 	exit -1
 end
 
@@ -10,24 +10,30 @@ path_file_addresses = ARGV[0]#"lista_enderecos.txt"
 path_file_pt = ARGV[1]#"pt.txt"
 
 # Create empty TLB
-tlb = Tlb.new 8
+tlb = Tlb.new ARGV[2].to_u64
 
 # Fill PT
 tlb.fillPt(path_file_pt)
 
+#print "\nMiss counter = #{tlb.missCounter}\n\n"
+#tlb.printTlb
+#print "\n"
+#tlb.printPt
+#tlb.printLRU()
+
 File.each_line path_file_addresses do |line|
-    address = line.to_u64(16)
-	#puts "#{address}"
+	address = line.to_u64(16)
+	#puts "#{line}"
     if tlb.translate(address) == -1
 		print "Endereco fora da pt\n"
 		exit -1
 	end
 
-	# gets
-	# print "\nMiss counter = #{tlb.missCounter}\n\n"
-	# tlb.printTlb
-	# print "\n"
-	# tlb.printPt
+	#gets
+	#print "\nMiss counter = #{tlb.missCounter}\n\n"
+	#tlb.printTlb
+	#print "\n"
+	#tlb.printPt
 	#tlb.printLRU()
 end
 
