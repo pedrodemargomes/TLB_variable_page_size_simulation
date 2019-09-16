08a40ff9045ab1e212fd46cb8b835b3ec6788418
Pedro Demarchi Gomes <pedrodemargomes@gmail.com>
2019-09-16 19:05:42 -0300

    fix add argument number of pmaps

--- pmap_transform/create_pmaps_numbered.sh
+++ pmap_transform/create_pmaps_numbered.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-if $# -ne 1
+if [ "$#" -ne 1 ]
+then
 	echo "numero errado de parametros"
 	echo "./create_pmaps_numbered.sh numero_de_pmaps"
-	exit
+	exit 1
 fi
 
 echo -n "" > pmaps.txt
-for i in {0..$1}
+for i in $(seq 0 $1)
 do
+	echo $i
 	file="pmap-${i}.txt"
 	while IFS= read -r line
 	do
